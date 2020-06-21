c8448022f30ce12dda7d674fc082e5c42e9c867c
cazuzaneves <cazuzaneves@gmail.com>
2020-06-21 14:51:02 -0300

    .

--- frontend/src/components/InputComp/index.tsx
+++ frontend/src/components/InputComp/index.tsx
@@ -1,12 +1,18 @@
-import React, { InputHTMLAttributes } from "react";
+import React, { InputHTMLAttributes, useState } from "react";
 import { Wrapper } from "./style";
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {}
 
 const InputComp = ({ ...inputProps }: Props) => {
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
-    <Wrapper>
-      <input {...inputProps} />
+    <Wrapper isFocused={isFocused}>
+      <input
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        {...inputProps}
+      />
     </Wrapper>
   );
 };
--- frontend/src/components/InputComp/style.ts
+++ frontend/src/components/InputComp/style.ts
@@ -1,21 +1,22 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const Wrapper = styled.div`
+interface WrapperProps {
+  isFocused: boolean;
+}
+
+export const Wrapper = styled.div<WrapperProps>`
   width: 320px;
   padding: 8px 16px;
   border: 2px solid #fff;
   border-radius: 4px;
   height: 40px;
   background: #fff;
+  border-color: ${({ isFocused, theme }) => isFocused && theme.darkOrange};
 
   > input {
     height: 100%;
     background: none;
     border: none;
     color: ${({ theme }) => theme.darkGray};
-  }
-
-  :focus {
-    border-color: ${({ theme }) => theme.darkOrange};
   }
 `;
